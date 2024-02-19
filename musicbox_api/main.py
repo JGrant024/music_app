07dca887cadc37a9b44c07f78ac5ba5920416995
JGrant024 <jonathan.grant23@icloud.com>
2024-02-19 09:54:00 -0500

    updated main.py to pull in songs from album

--- musicbox_api/main.py
+++ musicbox_api/main.py
@@ -1,18 +1,16 @@
 from sqlalchemy.ext.declarative import declarative_base 
 from fastapi import FastAPI 
 from fastapi.middleware.cors import CORSMiddleware
-# from models import Song
-# from sqlalchemy.orm import Session, sessionmaker
-# from sqlalchemy import create_engine
-
+from models import Song, Album
+from sqlalchemy import create_engine
+from db import session 
 
 
 
 app = FastAPI()
 Base = declarative_base()
 
-database_url = "postgresql://postgres:@localhost/Music_app"
-# engine = create_engine(database_url)
+
 
 # Setup our origins...
 # ...for now it's just our local environments
@@ -34,11 +32,30 @@ app.add_middleware(
 def home():
     return {"message": "Root Route"} 
 
-@app.get("/Jokes")
-def fetch_and_store_joke(): 
-    return{"message": "Testing new route"}
+# @app.get("/Jokes")
+# def fetch_and_store_joke(): 
+#     return{"message": "Testing new route"}
 
-# @app.get("/songs")
-# def get_songs(db: Session = sessionmaker(bind=engine)()):
-#     songs = db.query(Song).all()
-#     return songs
+@app.get("/songs")
+def get_songs():
+    songs = session.query(Song).all()
+    return songs
+
+@app.get("/songs/new")
+def add_songs():
+    album = session.query(Album).filter_by(title="RENAISSANCE").first()
+
+    song1 = Song(title="I'M THAT GIRL", album_id=album.id)
+
+    # song2 = Song(title="COZY", album=album)
+    # song3 = Song(title="ALIEN SUPERSTAR", album=album)
+    # song4 = Song(title="CUFF IT", album=album)
+    # song5 = Song(title="ENERGY (FEAT. BEAM)", album=album)
+    # song6 = Song(title="BREAK MY SOUL", album=album)
+    # song7 = Song(title="CHURCH GIRL", album=album)
+    # song8 = Song(title="PLASTIC OFF THE SOFA", album=album)
+    # song9 = Song(title="VIRGO'S GROOVE", album=album)
+    # song10 = Song(title="MOVE", album=album)
+    session.add_all ([song1])
+    session.commit() 
+    return{}
--- musicbox_api/models.py
+++ musicbox_api/models.py
@@ -5,7 +5,7 @@ from db import engine
 # from sqlalchemy import requests 
 
 Base = declarative_base()
-engine = create_engine("Music_app")
+
 
 class Joke(Base):
     __tablename__ = 'jokes'
@@ -39,29 +39,10 @@ class Song(Base):
     __tablename__ = 'songs'
     id = Column(Integer, primary_key=True)
     title = Column(String) 
-    album_id = Column(Integer, ForeignKey("album.id"))
+    album_id = Column(Integer, ForeignKey("albums.id"))
 
 # Create tables
 Base.metadata.create_all(engine)
 
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-album = session.query(Album).filter_by(title="RENAISSANCE").first()
-
-song1 = Song(title="I'M THAT GIRL", album=album)
-song2 = Song(title="COZY", album=album)
-song3 = Song(title="ALIEN SUPERSTAR", album=album)
-song4 = Song(title="CUFF IT", album=album)
-song5 = Song(title="ENERGY (FEAT. BEAM)", album=album)
-song6 = Song(title="BREAK MY SOUL", album=album)
-song7 = Song(title="CHURCH GIRL", album=album)
-song8 = Song(title="PLASTIC OFF THE SOFA", album=album)
-song9 = Song(title="VIRGO'S GROOVE", album=album)
-song10 = Song(title="MOVE", album=album)
-session.add_all ([song1,song2, song3, song4, song5, song6, song7, song8, song9, song10
-])
-
-session.commit() 
 # fetch_and_store_joke()
