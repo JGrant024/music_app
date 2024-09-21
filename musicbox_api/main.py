ba15f1c985def16122481c9f6d16427d0e804bb6
JGrant024 <jonathan.grant23@icloud.com>
2024-09-20 22:27:59 -0400

    Welcome Back!

--- musicbox_api/main.py
+++ musicbox_api/main.py
@@ -46,15 +46,18 @@ def add_songs():
     album = session.query(Album).filter_by(title="RENAISSANCE").first()
 
     song1 = Song(title="I'M THAT GIRL", album_id=album.id)
-    song2 = Song(title="COZY", album=album)
-    song3 = Song(title="ALIEN SUPERSTAR", album=album)
-    song4 = Song(title="CUFF IT", album=album)
-    song5 = Song(title="ENERGY (FEAT. BEAM)", album=album)
-    song6 = Song(title="BREAK MY SOUL", album=album)
-    song7 = Song(title="CHURCH GIRL", album=album)
-    song8 = Song(title="PLASTIC OFF THE SOFA", album=album)
-    song9 = Song(title="VIRGO'S GROOVE", album=album)
-    song10 = Song(title="MOVE", album=album)
+    song2 = Song(title="COZY",album_id=album.id)
+    song3 = Song(title="ALIEN SUPERSTAR",album_id=album.id)
+    song4 = Song(title="CUFF IT", album_id=album.id)
+    song5 = Song(title="ENERGY (FEAT. BEAM)", album_id=album.id)
+    song6 = Song(title="BREAK MY SOUL", album_id=album.id)
+    song7 = Song(title="CHURCH GIRL", album_id=album.id)
+    song8 = Song(title="PLASTIC OFF THE SOFA", album_id=album.id)
+    song9 = Song(title="VIRGO'S GROOVE", album_id=album.id)
+    song10 = Song(title="MOVE", album_id=album.id)
+
+
+    
     session.add_all ([song1,song2,song3,song4,song5,song6,song7,song8,song9,song10,])
     session.commit() 
     return{}
